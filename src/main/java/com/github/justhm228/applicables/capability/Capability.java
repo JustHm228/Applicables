1491111257282eeb6440578440954de25e4ddac3
JustHuman228 <justhuman228.github@gmail.com>
2025-06-08 20:13:22 +0300

    Create ``Capability.java``
    Create a ``Capability`` interface which allows to
    override ``onFinish(ApplicationContext)``, which
    was recently removed from ``Applicable``.

---
+++ src/main/java/com/github/justhm228/applicables/capability/Capability.java
@@ -0,0 +1,42 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2025 JustHuman228
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.justhm228.applicables.capability;
+
+import com.github.justhm228.applicables.Applicable;
+import com.github.justhm228.applicables.context.ApplicationContext;
+import com.github.justhm228.applicables.target.Targetable;
+
+public interface Capability<T extends Targetable<T>> extends Applicable<T> {
+
+	void onApply(final ApplicationContext<T> ctx);
+
+	void onFinish(final ApplicationContext<T> ctx);
+
+	@Override()
+	default void apply(final ApplicationContext<T> ctx) {
+
+		onApply(ctx);
+	}
+}
